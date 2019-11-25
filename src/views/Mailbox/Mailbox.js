f188adfe6fb262c02e5925abc71a2777a47ed87c
Nicolai Romero <anromerom@unal.edu.co>
2019-11-24 23:31:29 -0500

    Cambios a login, mensajes, etc

---
+++ src/views/Mailbox/Mailbox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
+//import {API} from 'API'
+
+const styles = theme => ({
+  root: {
+    padding: theme.spacing(3)
+  }
+});
+
+const MessageView = (  )=>{
+  return (<div/>);
+};
+
+export default withStyles(styles)(MessageView);
